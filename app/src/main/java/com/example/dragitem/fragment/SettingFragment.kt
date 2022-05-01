f16ef6f0145825883facf3671a2c74e410dd9907
MoonPrescene <95227833+MoonPrescene@users.noreply.github.com>
2022-05-02 06:57:27 +0700

    new commit 8:17 2/5/2022

--- app/src/main/java/com/example/dragitem/fragment/SettingFragment.kt
+++ app/src/main/java/com/example/dragitem/fragment/SettingFragment.kt
@@ -1,14 +1,14 @@
 package com.example.dragitem.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ListPopupWindow
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -21,13 +21,12 @@ class SettingFragment : Fragment() {
 
     private val viewModel: SettingFragmentViewModel by activityViewModels()
     private lateinit var checkCallButtonState: CheckCallButtonState
-
-    var buttonState = true
-    var sizeButton = 0
-
+    private var sizeButton = 0
     private lateinit var binding: FragmentSettingBinding
     private var buttonSizes = ButtonSize.values()
-    val TAG = "_BUTTON_SIZE_STATE"
+    private lateinit var listPopUpWindow: ListPopupWindow
+    private val TAG = "_BUTTON_SIZE_STATE"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +39,10 @@ class SettingFragment : Fragment() {
             false
         )
 
+        listPopUpWindow = ListPopupWindow(requireContext(),
+            null,
+            androidx.appcompat.R.attr.listPopupWindowStyle)
+
 
 
         //Get state of check call button state
@@ -47,16 +50,46 @@ class SettingFragment : Fragment() {
 
         activity?.actionBar?.hide()
 
-        val arrayAdapter = ArrayAdapter(
-            requireContext(),
-            R.layout.item_list_button_size,
-            buttonSizes.map {
-                it.sizeName
+        /*binding.apply {
+            buttonPopUp.setOnClickListener {
+                val popUp = PopupMenu(requireContext(), buttonPopUp)
+                popUp.inflate(R.menu.item_list)
+                popUp.setOnMenuItemClickListener {
+                    buttonPopUp.text = it.title
+                    getSizeOfButton()
+                    viewModel.setSize(sizeButton)
+                    true
+                }
+                popUp.show()
             }
-        )
-        binding.spinner.adapter = arrayAdapter
+        }*/
 
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        listPopUpWindow.anchorView = binding.buttonPopUp
+
+        val arrayAdapter = context?.let {
+            ArrayAdapter(
+                it,
+                R.layout.item_list_button_size,
+                buttonSizes.map {
+                    it.sizeName
+                }
+            )
+        }
+
+        listPopUpWindow.setAdapter(arrayAdapter)
+
+        listPopUpWindow.setOnItemClickListener { parent, view, position, id ->
+            sizeButton = buttonSizes[position].size
+            viewModel.setSize(sizeButton)
+            listPopUpWindow.dismiss()
+        }
+
+        binding.buttonPopUp.setOnClickListener {
+            v: View? -> listPopUpWindow.show()
+        }
+        //binding.spinner.adapter = arrayAdapter
+
+       /* binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 Log.d(TAG, "index of spinner: $position")
                 sizeButton = buttonSizes[position].size
@@ -68,7 +101,7 @@ class SettingFragment : Fragment() {
                 TODO("Not yet implemented")
             }
 
-        }
+        }*/
 
         binding.checkButton.setOnCheckedChangeListener { compoundButton, isChecked ->
             Log.d(TAG, "change button state: $isChecked")
@@ -82,24 +115,50 @@ class SettingFragment : Fragment() {
         return binding.root
     }
 
+    /*private fun getSizeOfButton() {
+        when(binding.buttonPopUp.text){
+            "Lớn" ->{
+                sizeButton = 0
+            }
+            "Trung bình" ->{
+                sizeButton = 1
+            }
+            "Nhỏ" ->{
+                sizeButton = 2
+            }
+        }
+    }*/
+
     private fun popToHomeFragment() {
         val action = SettingFragmentDirections.actionSettingFragmentToHomeFragment()
         findNavController().navigate(action)
     }
 
-    private fun showSizeOfButton(index: Int){
+   /* private fun showSizeOfButton(index: Int){
         binding.apply {
             spinner.post { spinner.setSelection(index)}
         }
-    }
+    }*/
 
+    @SuppressLint("SetTextI18n")
     private fun subscribeUi() {
         viewModel.apply {
             getCheckCallButtonState().observe(viewLifecycleOwner){
                 if (it != null){
                     Log.d(TAG, "lmao " + it.toMap())
                     checkCallButtonState = it
-                    showSizeOfButton(checkCallButtonState.size)
+//                    showSizeOfButton(checkCallButtonState.size)
+                    when(checkCallButtonState.size){
+                        0 -> {
+                            binding.buttonPopUp.text = "Lớn"
+                        }
+                        1 ->{
+                            binding.buttonPopUp.text = "Trung bình"
+                        }
+                        2 ->{
+                            binding.buttonPopUp.text = "Nhỏ"
+                        }
+                    }
                 }
             }
         }
@@ -108,7 +167,7 @@ class SettingFragment : Fragment() {
 }
 
 enum class ButtonSize(val sizeName: String = "", val size: Int ){
-    LARGE("Lon", 0),
-    MEDIUM("Trung binh", 1),
-    SMALL("Nho", 2)
+    LARGE("Lớn", 0),
+    MEDIUM("Trung bình", 1),
+    SMALL("Nhỏ", 2)
 }
